be955c5b53877ecc7af4ff6c3285d6ba39f409cf
Eric Martinez <eric.martinez@antit.io>
2023-11-16 18:24:56 -0300

    feat: header and prettier added

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'xcbou3bl',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Header() {
   return (
     <header>
-      <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
-        <Image
-          src="https://rb.gy/vsvv2o"
-          layout="fill"
-          objectFit="contain"
-          alt="apple logo"
-        />
+      <div className="flex items-center justify-center md:w-1/5">
+        <Link href="/">
+          <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
+            <Image
+              src="https://rb.gy/vsvv2o"
+              layout="fill"
+              objectFit="contain"
+              alt="apple logo"
+            />
+          </div>
+        </Link>
+      </div>
+
+      <div className="hidden flex-1 items-center justify-center space-x-8 md:flex">
+        <a className="headerLink">Product</a>
+        <a className="headerLink">Explore</a>
+        <a className="headerLink">Support</a>
+        <a className="headerLink">Business</a>
       </div>
     </header>
   );
