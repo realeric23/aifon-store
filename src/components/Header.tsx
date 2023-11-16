9ba88877569995c1b17dfbc9b8644eaaa545dd93
Eric Martinez <eric.martinez@antit.io>
2023-11-16 15:49:46 -0300

    feat: header + prettier added

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
+        <Image
+          src="https://rb.gy/vsvv2o"
+          layout="fill"
+          objectFit="contain"
+          alt="apple logo"
+        />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
